9b78c23c95d50e00da0c15b1e232a1da9414291d
Nigel Hennigar <nigelhennigar@hotmail.com>
2025-10-14 23:26:50 +0100

    built a more interesting network - pt 2

--- main.tf
+++ main.tf
@@ -16,26 +16,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-####################
-# Variables
-####################
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "eu-west-1"
-}
-
-variable "env" {
-  description = "Deployment environment to use for resources (dev, test, prod)"
-  type        = string
-  default     = "dev"
-}
-
-variable "allowed_ssh_cidr" {
-  description = "CIDR allowed to SSH into instances (replace with your IP/CIDR)"
-  type        = string
-  default     = "203.0.113.0/32"
-}
 
 ####################
 # Helpers / random suffix
--- variables.tf
+++ variables.tf
@@ -1,9 +1,20 @@
+####################
+# Variables
+####################
 variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-west-1"
+}
 
-  description = "AWS region"
+variable "env" {
+  description = "Deployment environment to use for resources (dev, test, prod)"
+  type        = string
+  default     = "dev"
+}
 
-  type = string
-
-  default = "us-east-1"
-
+variable "allowed_ssh_cidr" {
+  description = "CIDR allowed to SSH into instances (replace with your IP/CIDR)"
+  type        = string
+  default     = "203.0.113.0/32"
 }
